db5867bd632f9ec5a3bc117896bcd6c674f5265f
Devi <dvannanda@gmail.com>
2021-07-12 21:18:10 +0800

    updated ADM0-COUNTRY.R - add plot vignette

--- R/ADM0-COUNTRY.R
+++ R/ADM0-COUNTRY.R
@@ -9,21 +9,25 @@ library(sf)
 
 # get map data
 ID_MAP <- 
-  here("data/idn_adm_bps_20200401_shp/idn_admbndp_admALL_bps_itos_20200401.shp") %>% 
+  here("data",
+       "idn_adm_bps_20200401_shp",
+       "idn_admbnda_adm0_bps_20200401.shp") %>% 
   st_read()
 
-# combine polygons to ADM2
-ID_MAP_ADM0 <-
+# get center coordinates
+MAP_ADM0_xy <-
   ID_MAP %>% 
   select(ADM0_EN, geometry) %>% 
+  # to solve Error: `centroid` must be a data frame column
   group_by(ADM0_EN) %>% 
-  summarise(geometry = st_union(geometry)) %>% 
-  ungroup()
-
-# get coordinates
-MAP_ADM0_xy <-
-  ID_MAP_ADM0 %>% 
+  # get center coordinates
   mutate(centroid = map(geometry, ~setNames(st_centroid(.x), c("x", "y")))) %>%
-  unnest_wider(centroid)
+  unnest_wider(centroid) %>% 
+  ungroup() 
 
+# plot vignette
+MAP_ADM0_xy %>% 
+  ggplot() +
+  geom_sf(aes(geometry = geometry)) 
+  
 
